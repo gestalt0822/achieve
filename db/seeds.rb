bcb193c29e59bdfe2d645f2176ca38f18a489ba8
AOKI Daisuke <dice.k.aoki@gmail.com>
2016-11-28 06:24:42 +0000

    データベース再度作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+99.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(email:email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+n = 2
+while n <= 100
+  Blog.create(
+    title: "Dive into Coooooode #{n}",
+    user_id: n,
+    content: "#{n}人目のリレー投稿"
+    )
+    n = n + 1
+end
+
 n = 2
 while n <= 100
   user = User.find(n)
