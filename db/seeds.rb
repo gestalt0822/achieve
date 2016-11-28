914f72ee8bfc1df12fd0aa3df39aed8c75b9d0e4
AOKI Daisuke <dice.k.aoki@gmail.com>
2016-11-28 04:29:32 +0000

    ブログにユーザー名を追加、blogテーブルにサンプルデータ100件を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-100.times do |n|
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(email: email,
-               password: password,
-               password_confirmation: password,
-               )
+n = 2
+while n <= 100
+  user = User.find(n)
+  user.name = "ダミーユーザー#{n}"
+  user.save
+  n = n + 1
 end
