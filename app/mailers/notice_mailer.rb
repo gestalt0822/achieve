4287d46ba98732a230cc647508e40093c3226fb6
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2016-11-29 16:08:01 +0900

    (修正)メール送信機能

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -7,14 +7,12 @@ class NoticeMailer < ApplicationMailer
   #
   def sendmail_blog(blog)
     @blog = blog
-
     mail to: "dice.k.aoki@gmail.com",
          subject: '【Achieve】ブログが投稿されました'
   end
 
   def sendmail_contact(contact)
     @contact = contact
-
     mail to: "dice.k.aoki@gmail.com",
          subject: '【Achieve】お問い合わせが投稿されました'
   end
