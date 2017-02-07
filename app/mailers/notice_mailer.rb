9524b5575de7a9b07423012c19faa76ebc1903a5
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-02-07 11:40:33 +0900

    問い合わせ確認メール修正

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -13,7 +13,7 @@ class NoticeMailer < ApplicationMailer
 
   def sendmail_contact(contact)
     @contact = contact
-    mail to: "dice.k.aoki@gmail.com",
+    mail to: @contact.email,
          subject: '【Achieve】お問い合わせが投稿されました'
   end
 end
