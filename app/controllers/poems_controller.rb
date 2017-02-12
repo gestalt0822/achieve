32a66c643cfbb5f2607df07396f7b5b9888118e3
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-02-12 17:18:43 +0900

    API jsonになる問題を修正

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,11 +1,11 @@
 class PoemsController < ApplicationController
   def index
     @poems = Poem.all
-    render json: @poems
+    # render json: @poems
   end
 
   def show
     @poem = Poem.find(params[:id])
-    render json: @poem
+    # render json: @poem
   end
 end
