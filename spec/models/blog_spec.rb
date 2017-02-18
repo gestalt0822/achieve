c4933c5edc623b81ae766bb03da3482cec54be4e
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2017-02-18 16:59:51 +0900

    bin/rspac実施時のエラーを修正のため、再度bundle install

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 describe Blog do
   #タイトルがあれば有効な状態であること
   it "is valid with title"
+    blog = Blog.new(title: '宮岡', content: '暑いです')
+    expect(blog).to be_valid
 
   #タイトルがなければ無効であること
   it "is invalid without a title"
+    blog = Blog.new
+    expect(blog).not_to be_valid
 end
